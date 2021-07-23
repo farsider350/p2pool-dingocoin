b37a679d16bcc4bde99f10acb91a8965149cbef2
farsider350 <farsider350@github.com>
2021-07-23 11:59:43 +1000

    Test

--- p2pool/bitcoin/networks/dingocoin.py
+++ p2pool/bitcoin/networks/dingocoin.py
@@ -12,13 +12,13 @@ P2P_PORT = 33117
 ADDRESS_VERSION = 30
 RPC_PORT = 34646
 RPC_CHECK =RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-#            'dingocoin' in (yield bitcoind.rpc_help()) 
-            (yield helper.check_block_header(bitcoind, '1a91e3dace36e2be3bf030a65679fe821aa1d6ef92e7c9902eb318182c355691')) and
-                          (yield bitcoind.rpc_getblockchaininfo())['chain'] == 'main'
+            'dingocoin' in (yield bitcoind.rpc_help()) and
+#            (yield helper.check_block_header(bitcoind, '1a91e3dace36e2be3bf030a65679fe821aa1d6ef92e7c9902eb318182c355691')) and
+                          (yield bitcoind.rpc_getinfo())['chain'] == 'main'
         ))
 SUBSIDY_FUNC = lambda height: 88*100000000 >> (height + 1)//100000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
-BLOCK_PERIOD = 150 # s
+BLOCK_PERIOD = 60 # s
 SYMBOL = 'DINGO'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Dingocoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Dingocoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.dingocoin'), 'dingocoin.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'https://openchains.info/coin/dingocoin/'
